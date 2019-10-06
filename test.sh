facbecfb8970d907ba7f21cc5509de88c3846c71
Sonya <43223497+ssongyy@users.noreply.github.com>
2019-10-05 23:37:48 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -13,4 +13,4 @@ bash /mapreduce-test/start.sh
 /usr/local/hadoop/bin/hdfs dfs -cat /lab1/output/part-00000
 /usr/local/hadoop/bin/hdfs dfs -rm -r /lab1/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /lab1/output/
-bash/mapreuce-test/stop.sh
+bash /mapreuce-test/stop.sh
