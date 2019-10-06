ca663460d3f90d1378c727281f582522be7247d8
Sonya <43223497+ssongyy@users.noreply.github.com>
2019-10-05 20:23:10 -0400

    Add files via upload

---
+++ reducer2 .py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import re
+from collections import defaultdict
+top={}
+top = defaultdict(list)
+timee=[]
+for line in sys.stdin:
+    line=line.strip('\n')
+    num,ip=line.split('\t')
+    time=re.findall('([[0-9]+:[0-9]+])',ip)
+    value=re.findall(']([0-9].\S+[0-9])',ip)
+    for i in range(len(time)):
+        top[time[i]].append(value[i])
+    for time in top.keys():
+        if len(top[time])>=3 and time not in timee:
+            timee.append(time)
+            print(time,top[time])
+        else:
+            continue
+
