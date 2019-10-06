35572cb33cfd4946ef67d38cad0a172678aedcaf
Sonya <43223497+ssongyy@users.noreply.github.com>
2019-10-05 22:55:36 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,12 +3,12 @@
 /usr/local/hadoop/bin/hdfs dfs -rm -r /BigDataAsn/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /BigDataAsn/output/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /BigDataAsn/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../ssongyy/access.log.txt /BigDataAsn/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal /ssongyy/access.log.txt /BigDataAsn/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
--file ../ssongyy/BigDataAsn/mapper1.py -mapper ../ssongyy/BigDataAsn/mapper1.py \
--file ../ssongyy/BigDataAsn/reducer1.py -reducer ../ssongyy/BigDataAsn/reducer1.py \
--file ../ssongyy/BigDataAsn/mapper2.py -mapper ../ssongyy/BigDataAsn/mapper2.py \
--file ../ssongyy/BigDataAsn/reducer2 .py -reducer ../ssongyy/BigDataAsn/reducer2 .py \
+-file /BigDataAsn/mapper1.py -mapper ./ssongyy/BigDataAsn/mapper1.py \
+-file /BigDataAsn/reducer1.py -reducer ./ssongyy/BigDataAsn/reducer1.py \
+-file /BigDataAsn/mapper2.py -mapper ./ssongyy/BigDataAsn/mapper2.py \
+-file /BigDataAsn/reducer2 .py -reducer ./ssongyy/BigDataAsn/reducer2 .py \
 -input /BigDataAsn/input/* -output /BigDataAsn/output/
 /usr/local/hadoop/bin/hdfs dfs -cat /BigDataAsn/output/part-00000
 /usr/local/hadoop/bin/hdfs dfs -rm -r /BigDataAsn/input/
