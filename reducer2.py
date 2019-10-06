b96b4f5742f41633ef66be0e9f527d6f31e26be1
Sonya <43223497+ssongyy@users.noreply.github.com>
2019-10-06 18:55:32 -0400

    Update reducer2.py

--- reducer2.py
+++ reducer2.py
@@ -2,25 +2,29 @@
 # coding: utf-8
 
 # In[ ]:
-
-
 import re
 import sys
 from collections import defaultdict
 top={}
 top = defaultdict(list)
+h={}
+h=defaultdict(list)
 timee=[]
 for line in sys.stdin:
     line=line.strip('\n')
-    num,ip=line.split('\t')
-    time=re.findall('([[0-9]+:[0-9]+])',ip)
-    value=re.findall(']([0-9].\S+[0-9])',ip)
-    for i in range(len(time)):
-        top[time[i]].append(value[i])
+    line=line.split('\t')  
+    h[line[0]].append(line[1])
+sorted_dd = sorted(d.keys(),reverse=True)
+for k in sorted_dd:
+    for i in d[k]:
+        time=re.findall('([[0-9]+:[0-9]+])',i)
+        time=str(time)
+        value=re.findall(']([0-9]\S+)',i)
+        value=str(value)
+        top[time].append(value)
     for time in top.keys():
         if len(top[time])>=3 and time not in timee:
             timee.append(time)
             print(time,top[time])
         else:
             continue
-
