1f836ed4c476cc79f5d6a30b9a18362f4811a5a9
Sonya <43223497+ssongyy@users.noreply.github.com>
2019-10-07 13:11:23 -0400

    Update reducer4.py

--- reducer4.py
+++ reducer4.py
@@ -16,10 +16,10 @@ except:
 dict_ip_count = {}
 for line in sys.stdin:
     line = line.strip() ##移除字符的回车 变成了[00:00]5.108.86.176	1 strip(\n)的作用就是把本来一一行一行的，全部连在一起
-    ip, time, num = line.split('\t') ## 把空格去了split 的功能就是把空格去掉了ip 和number 之间的空格去掉了。变成了[00:00]5.108.86.176 1
+    ip, hour, num = line.split('\t') ## 把空格去了split 的功能就是把空格去掉了ip 和number 之间的空格去掉了。变成了[00:00]5.108.86.176 1
     ## 这一步就规定了两个变量在这个结构中，ip 和 number，然后取出空格， ip 指的就是hour+ip 地址， number 就是1
     try:
-        if int(start)>=int(time) and int(time)<int(end):
+        if int(start)<=int(hour) and int(hour)<int(end):
                 num = int(num)
                 dict_ip_count[ip] = dict_ip_count.get(ip, 0) + num
     except ValueError:
