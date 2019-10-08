7c28ad92ebf478792c49f0381c19a1e97988b674
Sonya <43223497+ssongyy@users.noreply.github.com>
2019-10-07 20:53:49 -0400

    Update reducer4.py

--- reducer4.py
+++ reducer4.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env python
 # coding: utf-8
 from operator import itemgetter
 import sys
@@ -6,14 +6,14 @@ dict_ip_count = {}
 start = sys.argv[1]
 end = sys.argv[2]
 for line in sys.stdin:
-    line = line.strip('\n') ##移除字符的回车 变成了[00:00]5.108.86.176	1 strip(\n)的作用就是把本来一一行一行的，全部连在一起
+    line = line.strip('\n') ##移除字符的回车 变成了[00:00]5.108.86.176  1 strip(\n)的作用就是把本来一一行一行的，全部连在一起
     ip, num = line.split('\t') ## 把空格去了split 的功能就是把空格去掉了ip 和number 之间的空格去掉了。变成了[00:00]5.108.86.176 1
     ## 这一步就规定了两个变量在这个结构中，ip 和 number，然后取出空格， ip 指的就是hour+ip 地址， number 就是1
     if int(ip[1:3])>=int(start) and int(ip[1:3])<int(end):
             num = int(num)
             dict_ip_count[ip] = dict_ip_count.get(ip, 0) + num
-        
+
 sorted_dict_ip_count = sorted(dict_ip_count.items(), key=itemgetter(1),reverse=True)[0:3]
 for ip, count in sorted_dict_ip_count:
-    print ('%s\t%s' % (ip, count))  
+    print ('%s\t%s' % (ip, count))
 
