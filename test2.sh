2fba36aa44046b19b8c10f60fcf4a28040768568
Sonya <43223497+ssongyy@users.noreply.github.com>
2019-10-07 20:02:34 -0400

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -8,7 +8,7 @@ bash /mapreduce-test/start.sh
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal /mapreduce-test/mapreduce-test-data/access.log /lab2/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
 -file ../BigDataAsn/mapper4.py -mapper ../BigDataAsn/mapper4.py \
--file ../BigDataAsn/reducer4.py -reducer '../BigDataAsn/reducer4.py  ${start} ${end}' \
+-file ../BigDataAsn/reducer4.py -reducer '../BigDataAsn/reducer4.py \
 -input /lab2/input/* -output /lab2/output/
 /usr/local/hadoop/bin/hdfs dfs -cat /lab2/output/part-00000
 /usr/local/hadoop/bin/hdfs dfs -rm -r /lab2/input/
